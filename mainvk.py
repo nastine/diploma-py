f912a42186623b300d57acca4d33335679016e33
nastine <nastine928@gmail.com>
2020-07-08 15:37:48 +0300

    Code improved, get photos fixed

--- mainvk.py
+++ mainvk.py
@@ -60,38 +60,64 @@ def get_people(access_token, sex, age_from, age_to, city_id, country_id):
         'has_photo': 1,
         'city': city_id,
         'country': country_id,
-        'is_closed': False
+        'is_closed': False,
+        'can_access_closed': False
 
     }
     result = api_request(URL, params)
     return result
 
 
-def get_country_id(country):
+def get_country_code():
+    user_country = input('Введите страну для поиска: ').capitalize()
+    with open('countries.json', 'r') as countries_file:
+        countries = json.load(countries_file)
+        if user_country not in countries.keys():
+            print('Страна введена неверно, попробуйте ещё раз.')
+            get_country_code()
+        else:
+            for country, code in countries.items():
+                if country == user_country:
+                    country_code = code
+                    return country_code
+
+
+def get_country_id():
+    country_code = get_country_code()
     URL = 'https://api.vk.com/method/database.getCountries'
-    params = {'v': '5.80', 'access_token': access_token, 'code': country}
+    params = {'v': '5.80', 'access_token': access_token, 'code': country_code}
     result = api_request(URL, params)
     return result['response']['items'][0]['id']
     
 
 
-def get_city_id(city, country_id):
+def get_city_id(country_id):
+    city = input('Введите желаемый город для поиска: ').capitalize()
     URL = 'https://api.vk.com/method/database.getCities'
     params = {'v': '5.80', 'access_token': access_token, 'country_id': country_id, 'q': city}
     result = api_request(URL, params)
-    return result['response']['items'][0]['id']
+    if result['response']['count'] == 0:
+        print('Город введен неверно, попробуйте ещё раз.')
+        get_city_id(country_id)
+    else:
+        return result['response']['items'][0]['id']
 
 
 def find_photos(owner_id):
     URL = 'https://api.vk.com/method/photos.get'
     params = {'v': '5.80', 'access_token': access_token, 'owner_id': owner_id, 'album_id': 'profile', 'extended': 1, 'count': 1000}
     result = api_request(URL, params)
-
     photos = {}
-    for items in result['response']['items']:
-        for size in items['sizes']:
-            if size['type'] == 'x':
-                photos[size['url']] = items['likes']['count']
+    try:
+        for items in result['response']['items']:
+            for size in items['sizes']:
+                if size['type'] == 'x':
+                    photos[size['url']] = items['likes']['count']
+    except KeyError:
+        if result['error']['error_code'] == 15:
+            print('Не удается загрузить фото, приватный профиль.')
+        else:
+            print(result)
 
     return sorted(photos.items(), key=lambda kv: kv[1], reverse=True)[0:3]
 
@@ -120,16 +146,46 @@ def write_result(people):
 
 
 def get_ten_users(people_db, n1, n2):
-    ten_users = people_db[n1:n2]
-    write_json(ten_users)
-    print('Результаты поиска записаны в json-файл.')
+    ten_users = ckeck_is_empty(people_db, n1, n2)
+    if ten_users != None: 
+        write_json(ten_users)
+        print('Результаты поиска записаны в json-файл.')
 
-    if input('Найти следующих 10 человек? (да/нет): ') == "да":
-        n1 += 10
-        n2 += 10
-        get_ten_users(people_db, n1, n2)
+        if input('Найти следующих 10 человек? (да/нет): ') == "да":
+            print('Поиск в процессе...')
+            n1 += 10
+            n2 += 10
+            get_ten_users(people_db, n1, n2)
+
+def check_age():
+    age = input('Введите диапазон возраста в формате "18-35": ')
+    age_from = age[:2]
+    age_to = age[-2:]
+    try:
+        int(age_from) >= int(age_to)
+        return age_from, age_to
+    except ValueError:
+        print('Введите чила')
+        check_age()
+    except TypeError:
+        print('Укажите диапазон возраста от меньшего к большему')
+        check_age() 
+
+def check_sex():
+    sex = input('Введите пол (1 - жен., 2 - муж., 0 - любой): ')
+    possible_vars = [1, 2, 0]
+    try:
+        if int(sex) in possible_vars:
+            return sex
+        else:
+            print('Укажите индекс одного из доступных вариантов (1, 2 или 0)')
+            check_sex()
+    except ValueError:
+        print('Укажите индекс одного из доступных вариантов (1, 2 или 0)')
+        check_sex()
 
 
+        
 
 def clear_my_db():
     client = MongoClient()
@@ -137,6 +193,30 @@ def clear_my_db():
     users = vk_db['users']
     vk_db.users.drop()
     return list(users.find())
+
+
+def ckeck_is_empty(people_db, n1, n2):
+    if not people_db[n1:n2]:
+        if input('По вашему запросу ничего не найдено, хотите изменить параметры поиска? ') == 'да':
+            main()
+    else: 
+        return people_db[n1:n2]
+
+
+def main():
+    country_id = get_country_id()
+    city_id = get_city_id(country_id)
+    sex = check_sex()
+    age_from, age_to = check_age()
+    print('Поиск в процессе...')
+    people = get_people(access_token, sex, age_from, age_to, city_id, country_id)
+    people_db = write_result(people)
+    n1, n2 = 0, 10
+    get_ten_users(people_db, n1, n2)
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -147,16 +227,8 @@ if __name__ == "__main__":
         get_token()
         access_token = input('Введите полученный токен для ВК: ')
     
-    country_id = get_country_id(input('Введите код желаемой страны для поиска (RU, UA, BY и т.д.): ').capitalize())
-    city_id = get_city_id(input('Введите желаемый город для поиска: ').capitalize(), country_id)
-    sex = input('Введите пол (1 - жен., 2 - муж., 0 - любой): ')
-    age = input('Введите диапазон возраста в формате "18-35": ')
-    age_from = age[:2]
-    age_to = age[-2:]
-    print('Поиск в процессе...')
-    people = get_people(access_token, sex, age_from, age_to, city_id, country_id)
-    people_db = write_result(people)
-    n1, n2 = 0, 10
-    get_ten_users(people_db, n1, n2)
+    main()
+    
+
 
     # print(clear_my_db())
--- test_vk.py
+++ test_vk.py
@@ -1,12 +1,23 @@
 import unittest
+from unittest.mock import patch
 import mainvk
 
 class TestVK(unittest.TestCase):
-    def test_get_country_id(self):
-        self.assertEqual(mainvk.get_country_id('RU'), 1)
+    @patch('builtins.input', lambda *args: 'Россия')
+    def get_country_code(self):
+        self.assertEqual(mainvk.get_country_code(), 1)
 
+    @patch('builtins.input', lambda *args: 'Москва')
     def test_get_city_id(self):
-        self.assertEqual(mainvk.get_city_id('Москва', '1'), 1)
+        self.assertEqual(mainvk.get_city_id('1'), 1)
+
+    @patch('builtins.input', lambda *args: 'жен.')
+    def test_check_sex(self):
+        self.assertRaises(ValueError)
+
+    @patch('builtins.input', lambda *args: 'двадцать-тридцать')
+    def test_ckeck_age(self):
+        self.assertRaises(ValueError)
 
     def test_invalid_token(self):
         self.assertRaises(KeyError)
